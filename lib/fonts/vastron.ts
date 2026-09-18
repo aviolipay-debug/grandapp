3a8b88c1c86d512d87deac6009f53163417640ca
aviolipay-debug <aviolipay@gmail.com>
2026-09-18 11:08:23 +0100

    Update vastron.ts

--- lib/fonts/vastron.ts
+++ lib/fonts/vastron.ts
@@ -1,7 +1,7 @@
 import localFont from "next/font/local";
 
 // Police sur-mesure "Vastron" (style ACHIKO) utilisée uniquement pour le
-// texte du logo "OliPay" (header mobile, header desktop, landing page).
+// texte du logo "Devisfinance" (header mobile, header desktop, landing page).
 export const vastron = localFont({
   src: "./Vastron.otf",
   variable: "--font-vastron",
